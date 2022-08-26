474fdc40091c5f6de7e8e09e3e21023410d9f9c2
itz-hamza <102425314+itz-hamza@users.noreply.github.com>
2022-08-26 21:01:03 +0500

    Update DMA.cpp

--- DMA.cpp
+++ DMA.cpp
@@ -1,2 +1,14 @@
 // dynamic stuff
+// dynamic stuff
+
+// what is the diffence between dynamic and static array ?
+
+// the new operator allocates the memory on heap and returns the adress of that newly allocated memory
+
+// when the new keyword is called for an object, first the memory is allocated
+// and then the object is initialized
+
+// a dynamic array is free sizeable, the static array has fixed size
+
+// to access anything from the heap, we must have a pointer
 
